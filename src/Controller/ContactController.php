efbc6469379de3adffce3d437e1cd9b5d7409c8b
erwan cloitre <erwan.cloitre@gmail.com>
2021-02-08 07:17:47 +0100

    contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\Contact1Type;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/contact")
+ */
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/send", name="contact")
+     * @param Request $request
+     * @return Response
+     */
+    public function send(Request $request): Response
+    {
+        // Create a new Category Object
+        $contact = new Contact();
+        // Create the associated Form
+        $form = $this->createForm(ContactType::class, $contact);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($contact);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('home');
+        }
+        // Render the form
+        return $this->render('send/send.html.twig', [
+            "contact" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/", name="contact_index", methods={"GET"})
+     */
+    public function index(ContactRepository $contactRepository): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'contacts' => $contactRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="contact_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(Contact1Type::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('contact_index');
+        }
+
+        return $this->render('contact/new.html.twig', [
+            'contact' => $contact,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="contact_show", methods={"GET"})
+     */
+    public function show(Contact $contact): Response
+    {
+        return $this->render('contact/show.html.twig', [
+            'contact' => $contact,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="contact_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Contact $contact): Response
+    {
+        $form = $this->createForm(Contact1Type::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('contact_index');
+        }
+
+        return $this->render('contact/edit.html.twig', [
+            'contact' => $contact,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="contact_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Contact $contact): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$contact->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($contact);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('contact_index');
+    }
+}
---
+++ src/Entity/Contact.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $Lastname;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $Firstname;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $mobile_number;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->Lastname;
+    }
+
+    public function setLastname(string $Lastname): self
+    {
+        $this->Lastname = $Lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->Firstname;
+    }
+
+    public function setFirstname(string $Firstname): self
+    {
+        $this->Firstname = $Firstname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getMobileNumber(): ?int
+    {
+        return $this->mobile_number;
+    }
+
+    public function setMobileNumber(int $mobile_number): self
+    {
+        $this->mobile_number = $mobile_number;
+
+        return $this;
+    }
+}
